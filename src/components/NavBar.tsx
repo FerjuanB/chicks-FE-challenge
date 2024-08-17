4eb60a8519f5dfae780484449e6e6f1fdfb3bf94
FerJuan <fbatres@gmail.com>
2024-08-17 00:06:53 -0300

    trabajando

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,39 +1,28 @@
+import { useState } from 'react';
+import { NavRefs } from "./NavComponents/NavRefs"
+
 export const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  
+
   return (
-<nav className="navBar">
-<button className="menu-toggle" aria-label="Abrir menú">
-                <span className="menu-icon"></span>
-            </button>
-    <img src="chicks-logo-large.svg" alt="chicks-logo" className="img-logo" />
-            <section className="nav-links">
-              <div>
-              <a href="#currency">currency</a>
-              </div>
-
-              <div>
-              <a href="#items">items</a>
-              </div>
-              <div>
-              <a href="#accounts">accounts</a>
-
-              </div>
-              <div>
-              <a href="#services">services</a>
-
-              </div>
-              <div>
-              <a href="#swap">services</a>
-
-              </div>
-              <div>
-              <a href="#sell">sell</a>
-
-              </div>
-              <div>
-              <a href="#usd">usd</a>
-
-              </div>
-
-            </section>
-</nav>  
-)}
+    <nav className={`navBar ${isMenuOpen ? 'menu-active' : ''}`}>
+      <button className="menu-toggle" aria-label="Abrir menú" onClick={()=>setIsMenuOpen(!isMenuOpen)}>
+        <span className="menu-icon"></span>
+      </button>
+      <img src="chicks-logo-large.svg" alt="chicks-logo" className="img-logo" />
+      <section className={`nav-links ${isMenuOpen ? 'show' : ''}`}>
+        <NavRefs value="currency"/>
+        <NavRefs value="items"/>
+        <NavRefs value="accounts"/>
+        <NavRefs value="services"/>
+        <NavRefs value="swap"/>
+        <NavRefs value="sell"/>
+      </section>
+      {/* <section className={` nav-right-links ${isMenuOpen ? 'show' : ''}`}>
+        <NavRefs value="USD "/>
+      </section> */}
+    </nav>  
+  )
+}
